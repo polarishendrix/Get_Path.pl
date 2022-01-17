10a2e49a9e809b4830b05c980cadc62d40759ba4
Polaris Hendrix <97896297+polarishendrix@users.noreply.github.com>
2022-01-17 16:29:28 -0600

    Create path.pl

---
+++ path.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# NAME: PATH.PL
+# DESIGNER: POLARIS HENDRIX
+# USE: DISPLAYS THE WINDOWS SYSTEM PATH
+# DATE: 01/17/2022
+
+# MUST USE BACKTICKS TO EXECUTE SYSTEM COMMAND
+my $path = `echo %path%`;
+
+# SPLITS THE PATH ENTRIES ON ";" AND PUTS INTO AN ARRAY
+my @array = split(';', $path);
+
+# PRINT EACH ELEMENT OF THE ARRAY
+foreach my $element (@array) {
+   print "$element\n";
+}
